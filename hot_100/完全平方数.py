9bbb8ab1e1ee6ac02b5c215628feac7ef6791e92
zhangmin <41320068+MinZhangm@users.noreply.github.com>
2020-07-15 22:44:31 +0800

    Create 完全平方数.py

---
+++ hot_100/完全平方数.py
@@ -0,0 +1,13 @@
+# 输入一个数字，返回和为这个数字的最少的完全平方数的个数
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        
+        dp = [float('inf') for _ in range(n+1)]
+        square = [i**2 for i in range(int(math.sqrt(n))+1)]
+        dp[0] = 0
+        for i in range(1, n+1):
+            for j in square:
+                if i - j >= 0:
+                    dp[i] = min(dp[i], dp[i-j]+1)
+        return dp[-1]
