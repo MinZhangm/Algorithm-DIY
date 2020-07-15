a9e1a18d733530cdec46c21a73599d55dff8dea1
zhangmin <41320068+MinZhangm@users.noreply.github.com>
2020-07-15 22:45:41 +0800

    Update 完全平方数.py

--- hot_100/完全平方数.py
+++ hot_100/完全平方数.py
@@ -8,6 +8,7 @@ class Solution:
         dp[0] = 0
         for i in range(1, n+1):
             for j in square:
-                if i - j >= 0:
-                    dp[i] = min(dp[i], dp[i-j]+1)
+                if i - j < 0:
+                    break
+                dp[i] = min(dp[i], dp[i-j]+1)                    
         return dp[-1]
