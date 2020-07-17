84628515cf1bc84c00586c86ad6bc2c50a71d463
zhangmin <41320068+MinZhangm@users.noreply.github.com>
2020-07-17 09:09:59 +0800

    Update *********字符串解码.py

--- hot_100/*********字符串解码.py
+++ hot_100/*********字符串解码.py
@@ -10,7 +10,7 @@ class Solution:
                 m = 0                       # 记录当前字符串的重复次数
                 res = ''                    # 记录当前这个[]内部的字符串内容
             elif s[i].isdigit():
-                m = m*10 + int(s[i])
+                m = m*10 + int(s[i])        # 注意这里，数字不都指示一位的，两位及以上的数字，由于是一位一位的读入，所以需要十进制处理
             elif s[i] == ']':
                 m_now, res_l = stack.pop()
                 res = res_l + m_now * res
